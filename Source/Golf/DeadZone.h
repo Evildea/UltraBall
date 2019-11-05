f3d8eda2f1adb02848d0d5620441a4f79fd6a564
Richard <richard@amikumu.com>
2019-11-05 12:18:11 +1100

    Made Changes For Sound Mapping

--- Source/Golf/DeadZone.h
+++ Source/Golf/DeadZone.h
@@ -32,6 +32,9 @@ public:
 	UPROPERTY(VisibleAnywhere)
 	class UPointLightComponent* Pointlight;
 
+	UPROPERTY(EditAnywhere, Category = "Designer")
+	class UAudioComponent* Sound;
+
 	UFUNCTION()
 	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 };
